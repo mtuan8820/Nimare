1c7fe22fbc4b562878421f8afaec902a13139a42
mtuan8820 <mtuan9517@gmail.com>
2025-07-25 19:31:23 +0700

    docs(vocabulary/minna): add vocabulary for lesson 38 docs(vocabulary/topic): add vocabulary topic countries' name

--- my-website/src/theme/MDXComponents.js
+++ my-website/src/theme/MDXComponents.js
@@ -2,9 +2,11 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
+import Flag from 'react-world-flags'
 
 export default {
     ...MDXComponents,
     Tabs,
-    TabItem
+    TabItem,
+    Flag
 };
